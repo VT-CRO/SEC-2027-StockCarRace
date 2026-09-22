7b290ee51a50e60fd33d0c07fcaef07daf804435
tylerthach2025 <tylerthach07@vt.edu>
2026-09-22 11:12:55 -0400

    Add enviroment setupfiles

---
+++ enviroment-setup/launch.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker run -it --user ros --name env-2027 -v $PWD/src:/home/ros/ws:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw --network=host --ipc=host --env=DISPLAY --gpus all vt-cro/ros-env
